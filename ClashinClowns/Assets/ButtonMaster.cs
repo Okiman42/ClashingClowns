5b9fe879d0b1bf83882a810dd2929447e5d6a1aa
Okiman42 <antonaxel05@gmail.com>
2023-09-18 11:05:13 +0200

    menu buttons
    have a play button that changes scens and a quit button that exits the application

--- ClashinClowns/Assets/ButtonMaster.cs
+++ ClashinClowns/Assets/ButtonMaster.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonMaster : MonoBehaviour
 {
@@ -14,5 +15,15 @@ public class ButtonMaster : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
