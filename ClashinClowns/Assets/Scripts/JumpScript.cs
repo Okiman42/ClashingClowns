46068bd581ba638c641cb0bcaee4b4a6b26b195b
Eldar073 <91468501+Eldar073@users.noreply.github.com>
2023-10-04 10:15:36 +0200

    JumpScript(WIP)

--- ClashinClowns/Assets/Scripts/JumpScript.cs
+++ ClashinClowns/Assets/Scripts/JumpScript.cs
@@ -1,11 +1,8 @@
 using UnityEngine;
 
-public class OtherMovementScript : MonoBehaviour
+public class JumpScript : MonoBehaviour
 {
-    private float horizontal;
-    [SerializeField] private float speed;
     [SerializeField] private float jumpingPower;
-    private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -24,27 +21,10 @@ public class OtherMovementScript : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
 
-        Flip();
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }
-
-    private void Flip()
-    {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
     }
 }
