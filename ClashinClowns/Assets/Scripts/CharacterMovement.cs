51c221e8fb21bb35d3c50e12089c09711f910f04
Eldar073 <91468501+Eldar073@users.noreply.github.com>
2023-10-11 09:37:28 +0200

    FlipingIsFixed

--- ClashinClowns/Assets/Scripts/CharacterMovement.cs
+++ ClashinClowns/Assets/Scripts/CharacterMovement.cs
@@ -7,14 +7,18 @@ public class CharacterMovement : MonoBehaviour
     public float moveSpeed = 5f; // Adjust this to control the character's movement speed.
 
     private Rigidbody2D rb;
+    private Transform characterTransform;
+    private bool facingRight = true;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        characterTransform = transform; // Cache the Transform component.
     }
 
     private void Update()
     {
+
         // Character 1 movement (using "A" and "D" keys).
         if (gameObject.tag == "Character1")
         {
@@ -22,6 +26,8 @@ public class CharacterMovement : MonoBehaviour
 
             Vector2 newVelocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
             rb.velocity = newVelocity;
+
+            FlipSprite(horizontalInput);
         }
         // Character 2 movement (using "UpArrow" and "DownArrow" keys).
         else if (gameObject.tag == "Character2")
@@ -30,6 +36,25 @@ public class CharacterMovement : MonoBehaviour
 
             Vector2 newVelocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
             rb.velocity = newVelocity;
+
+            FlipSprite(horizontalInput);
+        }
+
+    }
+
+    private void FlipSprite(float horizontalInput)
+    {
+        // If the character is moving right but not facing right, or moving left but not facing left, flip the sprite.
+        if ((horizontalInput > 0 && !facingRight) || (horizontalInput < 0 && facingRight))
+        {
+            // Toggle the boolean for facing direction.
+            facingRight = !facingRight;
+
+            // Calculate the new X-scale with a negative sign if moving left, or positive if moving right.
+            Vector3 newScale = characterTransform.localScale;
+            newScale.x *= -1;
+            characterTransform.localScale = newScale;
         }
     }
+
 }
